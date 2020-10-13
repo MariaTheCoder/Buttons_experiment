2448269d9ef9b2e2886eb8153dc7e97341fe34d8
Maria T. Andersen <mteilmann93@gmail.com>
2020-10-13 21:27:51 +0200

    apply function selectFontColor to set text color to random color

--- button.js
+++ button.js
@@ -1,11 +1,12 @@
-
 const buttonOne = document.getElementById("btn-1");
 const buttonTwo = document.getElementById("btn-2");
 
-buttonOne.addEventListener("click", function() {
-    let displayFirstEvent = document.getElementById("display_first_event"); 
-    let firstEvent = document.createElement("div");
-    displayFirstEvent.innerHTML = "";
-    firstEvent.innerHTML = "Hello World!";
-    displayFirstEvent.appendChild(firstEvent);
+buttonOne.addEventListener("click", function () {
+  let displayFirstEvent = document.getElementById("display_first_event");
+  let firstEvent = document.createElement("div");
+  displayFirstEvent.innerHTML = "";
+
+  displayFirstEvent.style.color = selectFontColor();
+  firstEvent.innerHTML = "Hello World!";
+  displayFirstEvent.appendChild(firstEvent);
 });
