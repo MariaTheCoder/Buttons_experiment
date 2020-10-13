032d031491cd8015c9488db811849f5e434ade82
Maria T. Andersen <mteilmann93@gmail.com>
2020-10-13 19:47:37 +0200

    First eventlistener added and implimented

---
+++ button.js
@@ -0,0 +1,10 @@
+
+const buttonOne = document.getElementById("btn-1");
+const buttonTwo = document.getElementById("btn-2");
+
+buttonOne.addEventListener("click", function() {
+    let displayFirstEvent = document.getElementById("display_first_event"); 
+    let firstEvent = document.createElement("div");
+    firstEvent.innerHTML = "Hello World!";
+    displayFirstEvent.appendChild(firstEvent);
+});
