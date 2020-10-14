7f91a0ef2068ba8a13f0f53e652f1f706beb3d5e
Maria T. Andersen <mteilmann93@gmail.com>
2020-10-14 23:10:59 +0200

    show rubberducky and play quack sound upon button click

--- button.js
+++ button.js
@@ -4,9 +4,23 @@ const buttonTwo = document.getElementById("btn-2");
 buttonOne.addEventListener("click", function () {
   let displayFirstEvent = document.getElementById("display_first_event");
   let firstEvent = document.createElement("div");
-  displayFirstEvent.innerHTML = "";
+  
+  resetDiv();
 
   displayFirstEvent.style.color = generateRandomColor();
   firstEvent.innerHTML = "Hello World!";
   displayFirstEvent.appendChild(firstEvent);
+});
+
+buttonTwo.addEventListener("click", function() {
+  let displaySecondEvent = document.getElementById("display_second_event");
+  // let secondEvent = document.createElement("div");
+  
+  resetDiv();
+
+  toggleImg("img");
+
+  quack();
+  
+  displaySecondEvent.appendChild(secondEvent);
 });
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
+function resetDiv() {
+  document.getElementById("display_first_event").innerHTML = "";
+  hideImg();
+}
 function generateRandomColor() {
   const fontDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
   let fontColor = "#";
@@ -10,4 +14,17 @@ function generateRandomColor() {
   }
 
   return fontColor;
+}
+function hideImg() {
+  document.getElementById("img").style.display = "none";
+}
+function toggleImg(imageId) {
+  if(document.getElementById(imageId).style.display === "none") {
+    return document.getElementById(imageId).style.display = "block";
+  }
+  document.getElementById(imageId).style.display = "none";
+}
+function quack() {
+  let sound = new Audio('./quack.mp3');
+  sound.play();
 }
